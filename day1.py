e58b48797fcdb7d0b802e302b9aaa49e8f714ef7
wilson7p <150508206+wilson7p@users.noreply.github.com>
2023-11-17 23:50:47 +0530

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,28 @@
+print(" wilson - Day 1 of #100DaysOfCode")
+
+#get user name and age and then print it
+print("\n program to get input and print output \n")
+name = input("Enter name: ")
+age = int(input("Enter age in integer:"))
+print("name name is ",name," and my age is ", age)
+
+#sum of two numbers
+print("\n addition")
+a=int(input())
+b=int(input())
+ans = a + b
+print("the sum of ",a ,b,"is ",ans)
+
+#product of two numbers
+print("\n multiplication")
+c=int(input())
+d=int(input())
+ans = c * d
+print("the product of ",c ,d,"is ",ans)
+
+#square root of two numbers
+print("\n square root")
+import math
+e=int(input())
+sqrtans = math.sqrt(e)
+print("square root of ",e," is ", sqrtans)
