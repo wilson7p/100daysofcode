51ca15e685868751032e448866acff1e4f0a03bf
wilson7p <150508206+wilson7p@users.noreply.github.com>
2023-11-18 21:19:24 +0530

    Update day2.py

--- day2.py
+++ day2.py
@@ -1,1 +1,7 @@
+print(" Wilson - Day 2 of #100DaysOfCode")
 
+#Celcius to Fahrenheit
+print("\n Celcius to Farenheit program\n")
+celcius = int(input("enter celcius value:"))
+farenheit = (celcius*9/5) + 32
+print("Farenheit of " + str(celcius) + "celcius is ",fahrenheit)
