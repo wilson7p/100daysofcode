43261d6a0fa9aa0973c21627a6c76b7c00fbe2b1
Wilson P <150508206+wilson7p@users.noreply.github.com>
2023-11-20 21:07:10 +0530

    Update day4.py

--- day4.py
+++ day4.py
@@ -1,1 +1,24 @@
+#Write a program to print the first 10 prime numbers. 
+print ("\nWilson - Day 4 of #100DaysOfCode\n")
+print("\nprint the first 10 prime numbers\n")
 
+def is_prime(num):
+    if num < 2:
+        return False
+    for i in range(2, int(num**0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
+
+def generate_primes(count):
+    primes = []
+    num = 2
+    while len(primes) < count:
+        if is_prime(num):
+            primes.append(num)
+        num += 1
+    return primes
+
+# Print the first 10 prime numbers
+first_10_primes = generate_primes(10)
+print("The first 10 prime numbers are:", first_10_primes)
