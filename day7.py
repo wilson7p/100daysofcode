b85f2eafd30b7b3e49b79fde0650c8e980093ae8
Wilson P <150508206+wilson7p@users.noreply.github.com>
2023-11-23 14:53:37 +0530

    Update day7.py

--- day7.py
+++ day7.py
@@ -1,1 +1,17 @@
+#Dictionaries and dictionaries manipulation
+print ("\nWilson - Day 7 of #100DaysOfCode\n")
+print("\nDictionaries and dictionaries manipulation\n")
 
+#creating a dictionary
+student = {'name': 'wilson','age':21,'marks':{'maths':90,'computer':97,'english':89},'courses':['maths','computer','english']}
+# Accessing dictionary values
+print("Student Name:", student['name'])
+print("Student Age:", student['age'])
+print("Maths mark:", student['marks']['maths'])
+print("courses:", ', '.join(student['courses']))
+
+keys = student.keys()
+values = student.values()
+
+print("\nKeys:", keys)
+print("Values:", values)
