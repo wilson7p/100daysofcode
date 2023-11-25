1c999a69496356d00baacc50093c757da21587a5
Wilson P <150508206+wilson7p@users.noreply.github.com>
2023-11-25 21:06:48 +0530

    Update day9.py

--- day9.py
+++ day9.py
@@ -1,1 +1,14 @@
-
+#Functions and function arguments
+print ("\nWilson - Day 9 of #100DaysOfCode\n")
+print("Functions and function arguments\n")
+#functions
+def greet(name, greeting="Hello"):
+    return f"{greeting}, {name}!"
+print(greet("Alice"))
+print(greet(name="Bob", greeting="Hi"))  
+print(greet("Charlie"))
+#function arguments
+def print_args(*args, **kwargs):
+    print("\nPositional arguments:", args)
+    print("Keyword arguments:", kwargs)
+print_args(1, 2, 3, a="apple", b="banana")
