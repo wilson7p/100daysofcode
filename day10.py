ece6d2f8582b94e3c04ab8153ba915b2626017e1
Wilson P <150508206+wilson7p@users.noreply.github.com>
2023-11-26 21:53:26 +0530

    Update day10.py

--- day10.py
+++ day10.py
@@ -1,1 +1,13 @@
+#Scope and lifetime of variables
+print("\nWilson - Day 10 of 100DaysOfCode")
+print("\nScope and lifetime of variables")
 
+total_sum = 0
+def totalfun(value):
+    global total_sum
+    total_sum += value
+    print(f"Inside the function - Added {value} to total_sum. New total_sum: {total_sum}")
+totalfun(5)
+totalfun(10)
+totalfun(8)
+print(f"Outside the function - Final total_sum: {total_sum}")
