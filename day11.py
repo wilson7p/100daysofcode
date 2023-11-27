a00007475831e1ce34556ed64c8ca49c33723c78
Wilson P <150508206+wilson7p@users.noreply.github.com>
2023-11-27 18:01:16 +0530

    Update day11.py

--- day11.py
+++ day11.py
@@ -1,1 +1,24 @@
+#Error handling (try, except)
+print("\nWilson - Day 11 of 100DaysOfCode")
+print("\n Error handling (try, except)")
+
+try:
+    x = int(input("Enter a number: "))
+    result = 10 / x
+    print("Result:", result)
+
+except ZeroDivisionError:
+    print("Error: Division by zero is not allowed.")
+
+except ValueError:
+    print("Error: Please enter a valid number.")
+
+except Exception as e:
+    print("An unexpected error occurred:", e)
+
+else:
+    print("No exceptions were raised.")
+
+finally:
+    print("This block always executes.")
 
