f73409c9c8048745af39f842f6a0c4484e70ff9c
Wilson P <150508206+wilson7p@users.noreply.github.com>
2023-11-28 18:39:10 +0530

    Update day12.py

--- day12.py
+++ day12.py
@@ -1,1 +1,12 @@
+#File I/O
+print("\nWilson - Day 12 of 100DaysOfCode")
+print("\n File I/O")
 
+#writing on a empty txt file
+with open('sampleio.txt','w') as file:
+  file.write('hello my name is wilson and im doing my 100daysofcode')
+with open('sampleio.txt', 'a') as file:
+  file.write('\na is used for appending some content in my sampleio txt file')
+with open('sampleio.txt','r') as file:
+  txt = file.read()
+  print(txt)
