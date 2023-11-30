6fa01ca4724ac93435d086e72849bd5e25387124
Wilson P <150508206+wilson7p@users.noreply.github.com>
2023-11-30 21:50:23 +0530

    Update day13.py

--- day13.py
+++ day13.py
@@ -1,1 +1,11 @@
+#Advanced list operations
+print("\nWilson - Day 13 of 100DaysOfCode")
+print("\nAdvanced list operations")
 
+list = [1, 2, 3, 4, 5]
+sq_list = [x**2 for x in list]
+print("sqared list values = ",sq_list)
+even = [x for x in sq_list if x % 2 == 0]
+print("even numbers in squared list = ",even)
+slice_list = sq_list[1:4]  
+print("elements from given index", slice_list)
