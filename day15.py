6354b167f3312e311b5c5d2ee5ca1000e20ad129
Wilson P <150508206+wilson7p@users.noreply.github.com>
2023-12-02 14:50:12 +0530

    Update day15.py

--- day15.py
+++ day15.py
@@ -1,1 +1,20 @@
+#loops
+print("\nWilson - Day 15 of 100DaysOfCode")
+print("\nloops")
+
+#while loop
+input = 1
+while input <= 10:
+     print(input)
+     input +=1
+
+#for loop
+pets = ['dog', 'cat', 'monkey', 'fish', 'snake']
+for pet in pets:
+    print(pet)
+for index, value in enumerate(pets):
+    print(f'{index}:{value}')
+print(max(pets))
+print(min(pets))
+print(sum(range(1, 4)))
 
