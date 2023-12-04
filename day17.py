5f2dd4fc2b036f7ec7f73cd1499d3fc5011be0cc
Wilson P <150508206+wilson7p@users.noreply.github.com>
2023-12-04 22:45:37 +0530

    Update day17.py

--- day17.py
+++ day17.py
@@ -1,1 +1,31 @@
+#Understanding generators and iterators
+print("\nWilson - Day 17 of 100DaysOfCode")
+print("\nUnderstanding generators and iterators")
 
+#iterator
+class Iterator:
+  def __iter__(self):
+    self.value = 1
+    return self
+
+  def __next__(self):
+    if self.value <= 5:
+      result = self.value
+      self.value += 1
+      return result
+    else:
+      raise StopIteration
+iter = Iterator()
+for item in iter:
+  print(item)
+
+#generrrator
+print("\ngenerator")
+def generator():
+    value = 1
+    while value <= 5:
+        yield value
+        value += 1
+gen = generator()
+for item in gen:
+    print(item)
