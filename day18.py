861082a56955fdebc940f597e769a957a013b75d
Wilson P <150508206+wilson7p@users.noreply.github.com>
2023-12-05 22:57:14 +0530

    Update day18.py

--- day18.py
+++ day18.py
@@ -1,1 +1,16 @@
+#Decorators and closures
+print("\nWilson - Day 18 of 100DaysOfCode")
+print("\nDecorators and closures")
+
+def my_decorator(func):
+    def wrapper():
+        print("BEFORE")
+        func()
+        print("AFTER")
+    return wrapper
+
+@my_decorator
+def say_hello():
+    print("WILSON")
+say_hello()
 
