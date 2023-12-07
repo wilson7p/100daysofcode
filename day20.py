508a7a3be825304ba5cf56c08a04730f6cba19fb
Wilson P <150508206+wilson7p@users.noreply.github.com>
2023-12-07 19:37:21 +0530

    Update day20.py

--- day20.py
+++ day20.py
@@ -1,1 +1,12 @@
+#Multiplication table of a number using nested loops.
+print("\nWilson - Day 20 of 100DaysOfCode")
+print("\n multiplication table of a number using nested loops")
 
+num = int(input("Enter value:"))
+range = range(1,11)
+print("Multiplication Table for ",num," is :")
+for i in range:
+  for j in range:
+    mul = i*j
+    print(mul,end ="")
+  print()
