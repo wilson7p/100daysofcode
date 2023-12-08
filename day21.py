e61193261e2b61372491c66852b2d6b66c3498a6
Wilson P <150508206+wilson7p@users.noreply.github.com>
2023-12-08 18:22:25 +0530

    Update day21.py

--- day21.py
+++ day21.py
@@ -1,1 +1,15 @@
+print("\nWilson - Day 21 of 100DaysOfCode")
+#Implement a program to check if a year is a leap year using if-else statements.
+print("\nleap year using if-else statements.")
 
+def leap(year):
+    if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+        return True
+    else:
+        return False
+check = int(input("Enter a year: "))
+
+if leap(check):
+    print(f"{check} is a leap year.")
+else:
+    print(f"{check} is not a leap year.")
