7cf29efd3de9dfc807979518fd683227f3a3b1aa
Wilson P <150508206+wilson7p@users.noreply.github.com>
2023-12-09 14:42:11 +0530

    Update day22.py

--- day22.py
+++ day22.py
@@ -1,1 +1,28 @@
+#Create a function to calculate the area of a rectangle.
+print("\nWilson - Day 21 of 100DaysOfCode")
+print("\nCalculate the area of a rectangle")
 
+def rarea():
+    length = float(input("enter length: "))
+    width = float(input("enter width: "))
+    area = length * width
+    return area
+
+result = rarea()
+print(f"The area of the rectangle is: {result}")
+
+#Write a function to check if a number is a perfect square.
+print("\n check perfect square")
+def is_square(number):
+  if number < 0:
+    return False
+  sqrt = int(number ** 0.5)
+  return sqrt ** 2 == number
+
+input = float(input("enter a number:"))
+sqresult = is_square(input)
+
+if result:
+  print(f"{input} is a perfect square")
+else:
+  print(f"{input} is not a perfect square")
