ac9ebb0b43b2cdad6ac7bf951ca6e948ee640698
Wilson P <150508206+wilson7p@users.noreply.github.com>
2023-12-11 20:31:43 +0530

    Update day24.py

--- day24.py
+++ day24.py
@@ -1,1 +1,12 @@
+#Implement a function to check if a string is a palindrome.
+print ("\nWilson - Day 24 of #100DaysOfCode\n") 
+print("\ncheck if a string is a palindrome\n")
+
+def is_palindrome(s):
+  clean = ''.join(s.split()).lower()
+  return clean == clean[::-1]
+input = input("enter sting: ")
+result = is_palindrome(input)
+print(f'Is "{input}" a palindrome? {result}')
+
 
