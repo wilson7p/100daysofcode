05733e79df810717050ecac6de01a46cf9f73cc3
Wilson P <150508206+wilson7p@users.noreply.github.com>
2023-12-15 15:04:19 +0530

    Update day28.py

--- day28.py
+++ day28.py
@@ -1,1 +1,19 @@
+#Write a program to count the frequency of elements in a list using a dictionary
+print ("\nWilson - Day 28 of #100DaysOfCode\n") 
+print("\ncount the frequency of elements in a list using a dictionary\n")
+
+def CountFrequency(list):
+  freq = {}
+  for item in list:
+    if(item in freq):
+      freq[item] += 1
+    else:
+      freq[item] = 1
+
+  for key, value in freq.items():
+    print("% d : % d" % (key, value))
+
+if __name__ == "__main__":
+  list = ['a','a','b','c','d','c','e','a','f']
+  CountFrequency(list)
 
