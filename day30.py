fb7bd99cac17bba2d06ec0adb397bf90005c7220
Wilson P <150508206+wilson7p@users.noreply.github.com>
2023-12-17 18:48:55 +0530

    Update day30.py

--- day30.py
+++ day30.py
@@ -1,1 +1,20 @@
+#Write a program to find the intersection of two sets.
+print ("\nWilson - Day 30 of #100DaysOfCode\n")
+print("\nfind the intersection of two sets.\n")
 
+def intersection(set1, set2):
+  result = set1.intersection(set2)
+  return result
+
+set1 = {1,2,3,4,5}
+set2 = {3,4,5,6,7}
+
+result = intersection(set1,set2)
+print(result)
+
+#Read a text file and print its content to the console.
+print("\nRead a text file and print its content to the console.")
+file_path = 'sample.txt'
+file = open(file_path,'r')
+content = file.read()
+print(content)
