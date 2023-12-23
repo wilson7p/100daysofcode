8a7ec0132bf6f3c29d8d24fdd76e3256e3329f0a
Wilson P <150508206+wilson7p@users.noreply.github.com>
2023-12-23 21:29:16 +0530

    Update day36.py

--- day36.py
+++ day36.py
@@ -1,1 +1,16 @@
+#Write a program that handles exceptions for division by zero.
+print ("\nWilson - Day 36 of #100DaysOfCode\n")
+print("\nhandles exceptions for division by zero.\n")
+
+def divide(a, b):
+  try:
+    result = a / b
+    print(f"{a} / {b} = {result}")
+  except ZeroDivisionError:
+    print("diving by zero not allowed")
+  
+numerator = int(input("numerator: "))
+dinominator = int(input("dinominator: ")) 
+
+divide(numerator, dinominator)
 
