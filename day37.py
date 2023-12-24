3b95fb3ca6a069a064f44514102e7548fc857b19
Wilson P <150508206+wilson7p@users.noreply.github.com>
2023-12-24 21:32:32 +0530

    Update day37.py

--- day37.py
+++ day37.py
@@ -1,1 +1,17 @@
+#Create a custom exception class and raise it in your code.
+print ("\nWilson - Day 37 of #100DaysOfCode\n")
+print("\ncustom exception class and raise it in your code.\n")
 
+class CustomError(Exception):
+  def __init__(self, message):
+    super().__init__(message)
+
+def example_function(number):
+  if number<0:
+    raise CustomError("negative numbers not allowed")
+
+try:
+  input = int(input("enter any number"))
+  example_function(input)
+except CustomError as ce:
+  print(f"Custom error caught: {ce}")
