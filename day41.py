1de66f448d023b572f314dab15714e10382580a1
Wilson P <150508206+wilson7p@users.noreply.github.com>
2023-12-28 22:11:11 +0530

    Update day41.py

--- day41.py
+++ day41.py
@@ -1,1 +1,16 @@
+#Use regular expressions to validate email addresses.
+print("\nWilson - Day 41 of #100DaysOfCode\n")
+print("\nregular expressions to validate email addresses.")
 
+import re
+
+def isvalid(email):
+  pattern = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
+  match = re.match(pattern, email)
+  return bool(match)
+
+inputemail = input("enter your email")
+if isvalid(inputemail):
+  print(f"{inputemail} is a valid email address")
+else:
+  print(f"{inputemail} is a not valid email address")
