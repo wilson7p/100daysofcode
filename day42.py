972ec32b104c7cdfa072dfa66a13026cab1cd761
Wilson P <150508206+wilson7p@users.noreply.github.com>
2023-12-29 21:31:25 +0530

    Update day42.py

--- day42.py
+++ day42.py
@@ -1,1 +1,24 @@
+#Implement a context manager for file handling.
+print ("\nWilson - Day 42 of #100DaysOfCode\n") 
+print("\nImplement a context manager for file handling.\n")
 
+class Filemanager:
+    def __init__(self, file_path, mode):
+        self.file_path = file_path
+        self.mode = mode
+        self.file = None
+
+    def __enter__(self):
+        self.file = open(self.file_path, self.mode)
+        return self.file
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        if self.file:
+            self.file.close()
+
+file_path = 'day42.txt'
+with Filemanager(file_path, 'w') as file:
+    file.write('im wilson')
+with Filemanager(file_path, 'r') as file:
+    content = file.read()
+    print(content)
