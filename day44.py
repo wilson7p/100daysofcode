860dab572aac172fa23e590af0d6938b2c712471
Wilson P <150508206+wilson7p@users.noreply.github.com>
2024-01-01 12:58:41 +0530

    Update day44.py

--- day44.py
+++ day44.py
@@ -1,1 +1,18 @@
+#Implement a basic web scraping program using a library like BeautifulSoup.
+print ("\nWilson - Day 44 of #100DaysOfCode\n") 
+print("\nImplement a basic web scraping program using a library like BeautifulSoup\n")
 
+import requests
+from bs4 import BeautifulSoup
+
+url = 'https://en.wikipedia.org/wiki/Main_Page'
+
+response = requests.get(url)
+
+if response.status_code == 200:
+    soup = BeautifulSoup(response.text, 'html.parser')
+    links = soup.find_all('a')
+    for link in links:
+        print(link.get('href'))
+else:
+    print(f"failed to fetch the page. Status code: {response.status_code}")
