f1a71cbbe3b469563e028a43d66b5daee19d3723
Wilson P <150508206+wilson7p@users.noreply.github.com>
2024-01-02 17:50:36 +0530

    Update day45.py

--- day45.py
+++ day45.py
@@ -1,1 +1,13 @@
+#Write a program to find the intersection of two sets.
+print ("\nWilson - Day 45 of #100DaysOfCode\n") 
+print("\nprogram to find the intersection of two sets\n")
 
+def fn_intersection(set1, set2):
+    intersection = set1 & set2
+    return intersection
+
+set1 = input("enter value for set 1 separated by comma: ")
+set2 = input("enter value for set 2 separated by comma: ")
+
+result = fn_intersection(set1, set2)
+print("Intersection of the sets:", result)
