2702ee3f9ea71f111bf7336a9a91ce7740d6d87d
Wilson P <150508206+wilson7p@users.noreply.github.com>
2024-01-03 11:52:16 +0530

    Update day46.py

--- day46.py
+++ day46.py
@@ -1,1 +1,65 @@
+#Implement a linked list data structure.
+print ("\nWilson - Day 46 of #100DaysOfCode\n") 
+print("\nImplement a linked list data structure\n")
 
+class Node:
+    def __init__(self, data=None):
+        self.data = data
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def is_empty(self):
+        return self.head is None
+
+    def append(self, data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+            return
+        last_node = self.head
+        while last_node.next:
+            last_node = last_node.next
+        last_node.next = new_node
+
+    def prepend(self, data):
+        new_node = Node(data)
+        new_node.next = self.head
+        self.head = new_node
+
+    def delete(self, data):
+        if self.head is None:
+            return
+
+        if self.head.data == data:
+            self.head = self.head.next
+            return
+
+        current_node = self.head
+        while current_node.next and current_node.next.data != data:
+            current_node = current_node.next
+
+        if current_node.next:
+            current_node.next = current_node.next.next
+
+    def display(self):
+        current_node = self.head
+        while current_node:
+            print(current_node.data, end=" -> ")
+            current_node = current_node.next
+        print("none")
+
+linked_list = LinkedList()
+linked_list.append(1)
+linked_list.append(2)
+linked_list.append(3)
+linked_list.prepend(0)
+
+print("original linked list: ")
+linked_list.display()
+linked_list.delete(2)
+print("linked list after deleting 2: ")
+linked_list.display()
