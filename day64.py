d92bd0f8277c9635cc13f7b8272c78d0ac49f463
Wilson P <150508206+wilson7p@users.noreply.github.com>
2024-01-23 23:03:47 +0530

    Update day64.py

--- day64.py
+++ day64.py
@@ -1,1 +1,21 @@
+#Diffie-Hellman.
+print ("\nWilson - Day 64 of #100DaysOfCode\n") 
+print("\nDiffie-Hellman\n")
 
+import random
+
+p=int(input())
+a=int(input())
+
+xa=random.randint(1,p-1)
+xb=random.randint(1,p-1)
+
+y_a=(a**xa)%p
+y_b=(a**xb)%p
+KA=(y_b**xa)%p
+KB=(y_a**xb)%p
+
+if(KA==KB):
+    print("Successfull")
+else:
+    print("Failed")
