488c4b032956bf65b1f40f1ad6e3c57b5b0a25df
Wilson P <150508206+wilson7p@users.noreply.github.com>
2024-01-24 11:12:09 +0530

    Update day65.py

--- day65.py
+++ day65.py
@@ -1,1 +1,26 @@
+#swapping between two integers.
+print ("\nWilson - Day 65 of #100DaysOfCode\n") 
+print("\nswapping between two integers\n")
 
+a = int(input("Enter the number:"))
+b = int(input("Enter the number:"))
+c = a^b
+d = c^b
+c = c^d
+print(c,d)
+print("\nXOR each character in this string\n")
+a = "Helloworld"
+for i in range(0, len(a)):
+  b = ord(a[i])
+  c = b^0
+  d = b^127
+  print(c,d)
+print("\none-time padding\n")
+a = input("enter a character:")
+b = ord(a)
+c = int(input("Enter the key value:"))
+d = b^c
+print("Cipher Text:",d)
+e = d^c
+print("Cipher Text xor with the key value:",e)
+print("The input message is:",chr(e))
