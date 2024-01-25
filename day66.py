ae524a94e952ea83864ba7c18e09682b1929a5ac
Wilson P <150508206+wilson7p@users.noreply.github.com>
2024-01-25 21:43:38 +0530

    Update day66.py

--- day66.py
+++ day66.py
@@ -1,1 +1,23 @@
+#ElGamal cryptography
+print ("\nWilson - Day 66 of #100DaysOfCode\n") 
+print("\nElGamal cryptography\n")
 
+import random
+from sympy import mod_inverse
+p=int(input())
+a=int(input())
+x=int(input())
+M=int(input())
+
+y=(a**x)%p
+
+k=int(input())
+K=(y**k)%p
+C1=(a**k)%p
+C2=(K*M)%p
+
+print(C1,C2)
+K_1=(C1**x)%p
+K_inv=mod_inverse(K_1,p)
+P=(C2*K_inv)%p
+print(P)
