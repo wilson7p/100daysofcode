f2e2ccf6e0a6d8e6334ab9afd40b3aef0cf8a644
Wilson P <150508206+wilson7p@users.noreply.github.com>
2024-01-28 11:59:03 +0530

    Update day67.py

--- day67.py
+++ day67.py
@@ -1,1 +1,31 @@
+#Asymmetric Cipher - Rivest-Shamir-Adleman.
+print ("\nWilson - Day 67 of #100DaysOfCode\n") 
+print("\nAsymmetric Cipher - Rivest-Shamir-Adleman\n")
 
+import random
+import math
+from sympy import mod_inverse, isprime
+
+def encrypt(message, e, n):
+    return (message ** e) % n
+def decrypt(cipher, d, n):
+    return (cipher ** d) % n
+
+p = int(input("Enter a prime number p: "))
+q = int(input("Enter another prime number q: "))
+n = p * q
+z = (p - 1) * (q - 1)
+e = random.randint(2, z - 1)
+while not isprime(e):
+    e = random.randint(2, z - 1)
+d = mod_inverse(e, z)
+message = int(input("Enter a message [Number] "))
+print()
+while math.gcd(message, n) != 1:
+    print("Please choose another message.")
+    message = int(input("Enter a message [Number] "))
+cipher_text = encrypt(message, e, n)
+decrypted_message = decrypt(cipher_text, d, n)
+print(f"Original Message: {message}")
+print(f"Cipher Text: {cipher_text}")
+print(f"Decrypted Message: {decrypted_message}")
